1d5fd1b0307be2a98a880f6af2da75473f6d605c
UnderTheCabinet <70901119+UnderTheCabinet@users.noreply.github.com>
2020-09-23 11:24:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function viewCurrDate() {
- document.getElementById('demo').innerHTML = Date();
+function viewCurrDate() {  eslint-disable-line no-unused-vars
+ document.getElementById('demo').innerHTML = Date(); alert("안녕하세요"); var name = prompt("이름을 입력하세요"); alert("입력한 이름은 " + name);
+ var ret = confirm("다시 입력하시겠습니까?"); alert(ret); //true or false 리턴
 }
