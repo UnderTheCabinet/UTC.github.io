0784c7d4e0111e4617a47f6a91860e26286c3326
UnderTheCabinet <70901119+UnderTheCabinet@users.noreply.github.com>
2020-09-28 12:32:53 +0900

    Create script0928.js

---
+++ script0928.js
@@ -0,0 +1,9 @@
+function text (){
+             
+            var x = 10;
+            var y = 20;
+            var z = x + y;
+            console.log(z);
+            document.write(z); 
+             
+         }
