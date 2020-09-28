ce4f90a2d25a01de00bb4d7e7369344383095524
UnderTheCabinet <70901119+UnderTheCabinet@users.noreply.github.com>
2020-09-29 01:00:54 +0900

    Update script0928.js

--- script0928.js
+++ script0928.js
@@ -3,7 +3,5 @@ function text (){
             var x = 10;
             var y = 20;
             var z = x + y;
-            console.log(z);
-            document.write(z); 
-             
+            alert(z);
          }
